07b083f28b94375ca39860e3ef7c0e6705596b1a
Suryadipto Sarkar <susarkar@BLR.ALLEGISINDIA.COM>
2016-09-19 14:38:14 +0530

    initial commit

---
+++ src/boot.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from "@angular/platform-browser";
+import {HelloPage} from "./hello.page";
+
+bootstrap(HelloPage);
---
+++ src/hello.page.ts
@@ -0,0 +1,12 @@
+import {bootstrap} from "@angular/platform-browser";
+import {Component} from "@angular/core";
+
+@Component({
+	selector:"app",
+	template:require("./hello.page.html")
+})
+
+export class HelloPage
+{
+	
+}
